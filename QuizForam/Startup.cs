40a59d1bcb98547034e035e7f8cf8d282a27cebb
sanjeev <sanjeev@SANJEEV-PC>
2017-02-01 09:52:26 +0530

    Add project files.

---
+++ QuizForam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuizForam.Startup))]
+namespace QuizForam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
